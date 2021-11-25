e6627857b0c6002318a93b5c070bea8404c4e7d8
Vineet <vineet.nagrath@gmail.com>
2021-11-25 09:49:17 +0100

    SmartAAS-DSL-v2 Released

---
+++ SmartAASbox/ImportJar.sh
@@ -0,0 +1,2 @@
+cp /home/nagrath/SOFTWARE/SmartAAS-GITs/SmartAAS-DSL/Java-BaSyx-Projects/smartmdsd2aas/target/smartmdsd2aas-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./smartmdsd2aas.jar
+cp /home/nagrath/SOFTWARE/SmartAAS-GITs/SmartAAS-DSL/Java-BaSyx-Projects/bridgesimulator/target/bridgesimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar ./bridgesimulator.jar
